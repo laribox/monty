e4f58bbf3fbad1f5a609ffc08cd46d5d30a292de
laribox <laribox2014@gmail.com>
2023-09-21 23:51:41 +0100

    Revert "fix"
    This reverts commit 2a27d4a729a5440f1a25eb8b55b8ffa2390cb22e.

--- pint.c
+++ pint.c
@@ -10,7 +10,8 @@ void f_pint(stack_t **head, unsigned int counter)
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(pv.file);
+		fclose(
+			.file);
 		free(pv.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
