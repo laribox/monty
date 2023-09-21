e39a53f26a1d563c9d7696ae09889f824538bbd8
laribox <laribox2014@gmail.com>
2023-09-22 00:09:53 +0100

    fixed execute problem

--- monty.h
+++ monty.h
@@ -54,9 +54,7 @@ typedef struct program_args
 }  pro_args;
 extern pro_args pa;
 
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
+
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
