070eed427535a7db17bd10ab1a6b8a761bf730f3
laribox <laribox2014@gmail.com>
2023-09-22 00:23:48 +0000

    fixed

--- execute.c
+++ execute.c
@@ -9,9 +9,9 @@
  */
 int parse_line(char *content, char **op, char **arg)
 {
-  	*op = strtok(content, " \n\t");
+	*op = strtok(content, " \n\t");
 	if (*op && (*op)[0] == '#')
-	  	return (0);	
+		return (0);	
 	*arg = strtok(NULL, " \n\t");
 	return (*op != NULL);
 }
@@ -26,31 +26,31 @@ int parse_line(char *content, char **op, char **arg)
  */
 void execute_opcode(char *op, char *arg, stack_t **stack, unsigned int counter, FILE *file)
 {
-    instruction_t opst[] = {
-        {"push", push},
-        {"pall", pall}, {"pint", pint},
-        {"pop", pop},
-        {"swap", swap},
-        {"add", add},
-        {"nop", nop},
-        {"sub", sub},
-        {"div", div_s},
-        {"mul", mul},
-        {"mod", mod},
-        {NULL, NULL}
-    };
-    unsigned int i = 0;
+	instruction_t opst[] = {
+		{"push", push},
+		{"pall", pall}, {"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", div_s},
+		{"mul", mul},
+		{"mod", mod},
+		{NULL, NULL}
+	};
+	unsigned int i = 0;
 
-    while (opst[i].opcode)
-    {
-        if (strcmp(op, opst[i].opcode) == 0)
-        {
-            if (opst[i].f == push && arg == NULL)
-            {
-                fprintf(stderr, "L%d: usage: push integer\n", counter);
-                fclose(file);
-                free_stack(*stack);
-                exit(EXIT_FAILURE);
+	while (opst[i].opcode)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{
+			if (opst[i].f == push && arg == NULL)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", counter);
+				fclose(file);
+				free_stack(*stack);
+				exit(EXIT_FAILURE);
             }
             opst[i].f(stack, counter);
             return;
